16a436f82714cc694377c88af5d154edf36ecd4e
Shrimathi D <shrimd17@in.ibm.com>
2019-08-21 11:59:11 +0530

    Updates

---
+++ precache-manifest.9d2219a1bcee6752fc93209182fdb39d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ed794b36d98207f2a7e184729ea9db",
+    "url": "/calci/index.html"
+  },
+  {
+    "revision": "cde2d758956b1ac45cea",
+    "url": "/calci/static/css/2.de55ad5b.chunk.css"
+  },
+  {
+    "revision": "acd1dfc5c76913dd68b5",
+    "url": "/calci/static/css/main.dd4bdf98.chunk.css"
+  },
+  {
+    "revision": "cde2d758956b1ac45cea",
+    "url": "/calci/static/js/2.a331f423.chunk.js"
+  },
+  {
+    "revision": "acd1dfc5c76913dd68b5",
+    "url": "/calci/static/js/main.0c8c58ac.chunk.js"
+  },
+  {
+    "revision": "7a2c9a346157e4950b18",
+    "url": "/calci/static/js/runtime~main.f3da3e60.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/calci/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/calci/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/calci/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/calci/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/calci/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
